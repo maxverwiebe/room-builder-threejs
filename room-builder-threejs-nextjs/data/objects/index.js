99980f166301acdc921814c800dd6152c9db6b65
maxverwiebe <summe.business@gmail.com>
2025-03-15 15:20:24 +0100

    Add CAUSign mesh/object

--- room-builder-threejs-nextjs/data/objects/index.js
+++ room-builder-threejs-nextjs/data/objects/index.js
@@ -3,3 +3,4 @@ export { default as hellokitty } from "./hellokitty";
 export { default as table } from "./table";
 export { default as whiteboard } from "./whiteboard";
 export { default as table2 } from "./table2";
+export { default as cauSign } from "./cauSign";
