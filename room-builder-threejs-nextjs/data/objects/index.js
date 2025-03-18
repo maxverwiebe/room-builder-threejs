dba9ca01ddcef5725bbcc4055ae365510f7c9bc2
maxverwiebe <summe.business@gmail.com>
2025-03-18 13:23:27 +0100

    Add seatbank mesh

--- room-builder-threejs-nextjs/data/objects/index.js
+++ room-builder-threejs-nextjs/data/objects/index.js
@@ -5,3 +5,4 @@ export { default as whiteboard } from "./whiteboard";
 export { default as table2 } from "./table2";
 export { default as cauSign } from "./cauSign";
 export { default as trashbin } from "./trashbin";
+export { default as seatbank } from "./seatbank";
